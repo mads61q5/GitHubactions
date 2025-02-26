531599704a03953d04016ec2d3c6f88e1bfd8af4
Mads Engstrøm <mads61q5@stud.kea.dk>
2025-02-26 08:42:41 +0100

    Initial commit

---
+++ src/main/java/org/example/githubactions/GitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsApplication.class, args);
+    }
+
+}
